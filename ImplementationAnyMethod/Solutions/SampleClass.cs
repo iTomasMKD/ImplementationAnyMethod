b0f0d1efadcc477486e8541b4504ee36108b0d2e
Ilijevski7 <tomche.kubikos@hotmail.com>
2022-07-30 12:57:10 +0200

    Add project files.

---
+++ ImplementationAnyMethod/Solutions/SampleClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImplementationAnyMethod.Solutions
+{
+    public class SampleClass
+    {
+        //Using Count() enumerates the entire collection to compute the count
+        public void BadSampleMethod(IEnumerable<string> employees)
+        {
+            if (employees is null || employees.Count() == 0)
+            {
+                return;
+
+                // other implementations
+            }
+        }
+
+        //Using Any() method is faster and more readable
+        public void GoodSampleMethod(IEnumerable<string> employees)
+        {
+            if (employees is null || !employees.Any())
+            {
+                return;
+            }
+        }
+    }
+}
